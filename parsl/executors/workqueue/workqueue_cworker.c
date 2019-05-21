8c3c23d5c164de4251c873e99a9d699cc0c8a504
MalcolmUnWang <unsolvab@mail.ustc.edu.cn>
2019-05-21 11:14:36 -0500

    modify

--- parsl/executors/workqueue/workqueue_cworker.c
+++ parsl/executors/workqueue/workqueue_cworker.c
@@ -1,10 +1,11 @@
 #include<zmq.h>
 #include<string.h>
 #include<stdio.h>
-#include<stdlib.h>
 
 
 int main(int argc, char *argv[]){
+
+    printf("Output");
     int index = 1;
     char * input_function_file;
     char * output_function_file;
@@ -18,28 +19,51 @@ int main(int argc, char *argv[]){
             index += 1;
         }
         else{
-            printf("Command is not supported");
+            printf("Command is not supported %d\n",index);
             return 2;
         }
         index++;
     }
+    printf("Output");
 
     void *context = zmq_ctx_new ();
-    void *requester = zmq_socket (context, ZMQ_REQ);
-    int rc = zmq_bind (requester, "tcp://localhost:5555");
+    printf("Output");
 
+    void * requester = zmq_socket(context, ZMQ_REQ);
+    printf("Output");
+    int rc = zmq_connect(requester, "tcp://localhost:5556");
+
+    zmq_send (requester, input_function_file, strlen(input_function_file), 0);
 
     char buffer[1000];
 
-    zmq_send(requester, input_function_file, strlen(input_function_file),0);
+    printf("Output");
 
-    zmq_recv(requester, buffer, 1000,0);
+    zmq_recv (requester, buffer, 1000, 0);
 
-    FILE * fp;
+    FILE *fp;
     fp = fopen(output_function_file, "wb");
-    fputs(buffer,fp);
+
+    fprintf(fp,"%s" ,buffer);
+
     fclose(fp);
+    zmq_close (requester);
+    zmq_ctx_destroy (context);
+    return 0;
+
+    /*
+    zmq::message_t request(std::strlen(input_function_file));
+    memcpy(request.data(), input_function_file, std::strlen(input_function_file));
+
+    requester.send(request);
+
+    zmq::message_t result;
+    requester.recv(&result);
+
+    std::fstream outputfile = std::fstream(output_function_file, std::ios::out | std::ios::binary);
+    outputfile.write(static_cast<char*>(result.data()),result.size());
 
     return 0;
+    */
 
 }
